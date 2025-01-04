78d2dcb3f785cbb37021584bb9369d99caa46ff2
Vitor Franca <vitorfranca089@gmail.com>
2025-01-04 20:35:45 -0300

    feat: optimize memory usage and performance

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        StringBuffer stringBuffer = new StringBuffer();
 
         int sizePass = scanner.nextInt();
+        StringBuffer stringBuffer = new StringBuffer(sizePass);
 
         int[] options = new int[4];
 
@@ -24,10 +24,13 @@ public class Main {
 
         for(int i = 0; i < options.length; i++) if(options[i] == 1) chosenCharset.add(charset[i]);
 
+        int randomCharsetIndex;
+        int randomCharIndex;
+
         for(int i = 0; i < sizePass; i++){
-            char[] charsetRand = chosenCharset.get(random.nextInt(chosenCharset.size()));
-            int charRand = random.nextInt(charsetRand.length);
-            stringBuffer.append(charsetRand[charRand]);
+            randomCharsetIndex = random.nextInt(chosenCharset.size());
+            randomCharIndex = random.nextInt(chosenCharset.get(randomCharsetIndex).length);
+            stringBuffer.append(chosenCharset.get(randomCharsetIndex)[randomCharIndex]);
         }
 
         String password = stringBuffer.toString();
